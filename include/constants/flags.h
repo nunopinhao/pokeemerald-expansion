404729cf0ed2aa9c2b4c449515eae184bb0879bf
Nuno <nuno.bras.pinhao@tecnico.ulisboa.pt>
2025-09-15 20:15:33 +0100

    Change flag names for readability

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -43,9 +43,9 @@
 #define TEMP_FLAGS_END   FLAG_TEMP_1F
 #define NUM_TEMP_FLAGS   (TEMP_FLAGS_END - TEMP_FLAGS_START + 1)
 
-#define FLAG_UNUSED_0x020    0x20 // Unused Flag
-#define FLAG_UNUSED_0x021    0x21 // Unused Flag
-#define FLAG_UNUSED_0x022    0x22 // Unused Flag
+#define FROAKIE_STARTER_FLAG    0x20 // Unused Flag
+#define ROWLET_STARTER_FLAG    0x21 // Unused Flag
+#define TORCHIC_STARTER_FLAG    0x22 // Unused Flag
 #define FLAG_UNUSED_0x023    0x23 // Unused Flag
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
 #define FLAG_UNUSED_0x025    0x25 // Unused Flag
