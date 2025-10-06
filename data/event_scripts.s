030974438e2533c167dd9bdecaf0e9c9a5e79331
Nuno <nuno.bras.pinhao@tecnico.ulisboa.pt>
2025-10-06 17:49:53 +0100

    Add mr diamond house

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1202,3 +1202,5 @@ EventScript_VsSeekerChargingDone::
 	.include "data/maps/Route04/scripts.inc"
 
 	.include "data/maps/Route05/scripts.inc"
+
+	.include "data/maps/Route04_MrDiamondHouse/scripts.inc"
